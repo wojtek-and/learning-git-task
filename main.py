e755e2700f0f4f193004b0b4a272de675377ab5f
Wojciech Andrzejuk <wojciech.andrzejuk@wp.pl>
2024-04-19 16:36:21 +0200

    Add loop

--- main.py
+++ main.py
@@ -3,3 +3,6 @@ lista_zakupów = {
     "warzywniak": ["marchew", "seler", "rukola"]
 }
 
+for shop, products in lista_zakupów.items():
+    product_capital = [product.capitalize() for product in products]
+    print(f"Idę do {shop.capitalize()} i kupuję tu następujące rzeczy: {product_capital}.")
