9bc07f3caf9f80c457cfe424447f5816d2f3e0ae
Wojciech Andrzejuk <wojciech.andrzejuk@wp.pl>
2024-04-19 16:39:33 +0200

    Add quantity of products

--- main.py
+++ main.py
@@ -6,3 +6,5 @@ lista_zakupów = {
 for shop, products in lista_zakupów.items():
     product_capital = [product.capitalize() for product in products]
     print(f"Idę do {shop.capitalize()} i kupuję tu następujące rzeczy: {product_capital}.")
+
+print(f"W sumie kupuję {len(lista_zakupów['piekarnia']) + len(lista_zakupów['warzywniak'])} produktów.")
